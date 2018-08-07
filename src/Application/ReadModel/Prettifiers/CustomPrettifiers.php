c91eb01026042017f8ce878c7e8ba7dd2130909d
Holger Woltersdorf <hw@hollo.me>
2018-08-08 01:07:11 +0200

    Add untyped collection with PrettifiesString interface for custom prettifiers, #17

---
+++ src/Application/ReadModel/Prettifiers/CustomPrettifiers.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace hollodotme\Readis\Application\ReadModel\Prettifiers;
+
+use hollodotme\Readis\Application\ReadModel\Interfaces\PrettifiesString;
+
+final class CustomPrettifiers implements PrettifiesString
+{
+	/** @var array|PrettifiesString[] */
+	private $prettifiers = [];
+
+	public function addPrettifiers( ...$prettifiers ) : void
+	{
+		foreach ( $prettifiers as $prettifier )
+		{
+			$this->prettifiers[] = $prettifier;
+		}
+	}
+
+	public function canPrettify( string $data ) : bool
+	{
+		return true;
+	}
+
+	public function prettify( string $data ) : string
+	{
+		foreach ( $this->prettifiers as $prettifier )
+		{
+			if ( $prettifier->canPrettify( $data ) )
+			{
+				return $prettifier->prettify( $data );
+			}
+		}
+
+		return $data;
+	}
+}
